e413e250a035e1b96cda43711dfb0da9c0972877
20211011110010 <20211011110010@CNAT391775>
2022-10-17 15:55:50 -0300

    Adicionar arquivos de projeto.

---
+++ AgendaApp/Contato.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaApp
+{
+    class Contato
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public Contato(string n, string e, string p)
+        {
+            Name = n;
+            Email = e;
+            Phone = p;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Email} - {Phone}";
+        }
+    }
+}
---
+++ AgendaApp/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AgendaApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OnSubmeterClick(object sender, RoutedEventArgs e)
+        {
+            Contato c = new Contato(nameField.Text, emailField.Text, phoneField.Text);
+            listBox.Items.Add(c);
+        }
+
+        private void OnRemoverClick(object sender, RoutedEventArgs e)
+        {
+            if (listBox.SelectedItem != null)
+            {
+                listBox.Items.Remove(listBox.SelectedItem);
+            }
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+}
